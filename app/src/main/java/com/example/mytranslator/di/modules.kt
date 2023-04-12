29195fcff3115915e51e24743134c2e052d125f1
geekbrains <info@geekbrains.ru>
2023-04-12 18:29:37 +0300

    Lesson 7

--- app/src/main/java/com/example/mytranslator/di/modules.kt
+++ app/src/main/java/com/example/mytranslator/di/modules.kt
@@ -3,9 +3,13 @@ package com.example.mytranslator.di
 import com.example.mytranslator.view.history.HistoryFragmentViewModel
 import com.example.mytranslator.view.search.MainFragmentViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val mainKoinModule = module {
+    scope(named("GREETING")) {
+        scoped { "Hello, user!" }
+    }
     viewModel { MainFragmentViewModel(application = get(), repo = get(), localRepo = get()) }
     viewModel { HistoryFragmentViewModel(localRepo = get()) }
 }
--- utils/src/main/java/com/example/utils/ViewUtils.kt
+++ utils/src/main/java/com/example/utils/ViewUtils.kt
@@ -1,7 +1,12 @@
 package com.example.utils
 
+import android.app.Activity
 import android.view.View
+import androidx.annotation.IdRes
+import androidx.fragment.app.Fragment
 import com.google.android.material.snackbar.Snackbar
+import java.lang.ref.WeakReference
+import kotlin.reflect.KProperty
 
 fun View.show() {
     visibility = View.VISIBLE
@@ -24,4 +29,38 @@ fun makeSnackbar(
         .setAction(actionText, action)
         .setAnchorView(anchor)
         .show()
+}
+
+class ViewByIdDelegate<out T : View>(private val rootGetter: () -> View?, private val viewId: Int) {
+    private var rootRef: WeakReference<View>? = null
+    private var viewRef: T? = null
+
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): T {
+        var view = viewRef
+        val cachedRoot = rootRef?.get()
+        val currentRoot = rootGetter()
+
+        if (currentRoot != cachedRoot || view == null) {
+            if (currentRoot == null) {
+                if (view != null) {
+                    return view
+                }
+                throw IllegalStateException("Cannot get View, there is no root yet")
+            }
+            view = currentRoot.findViewById(viewId)
+            viewRef = view
+            rootRef = WeakReference(currentRoot)
+        }
+
+        checkNotNull(view) { "View with id \"$viewId\" not found in root" }
+        return view
+    }
+}
+
+fun <T : View> Activity.viewById(@IdRes viewId: Int): ViewByIdDelegate<T> {
+    return ViewByIdDelegate({ window.decorView.findViewById(android.R.id.content) }, viewId)
+}
+
+fun <T : View> Fragment.viewById(@IdRes viewId: Int): ViewByIdDelegate<T> {
+    return ViewByIdDelegate({ view }, viewId)
 }
